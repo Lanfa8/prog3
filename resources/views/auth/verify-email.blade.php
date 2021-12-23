41e774e6d0ca1c1b02e96aa4c87a6724b67b1638
Lanfa <alanfan139@gmail.com>
2021-12-22 21:27:14 -0300

    melhorias css página de email

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -1,11 +1,10 @@
 @extends('templates.base')
 @section('content')
 
-<div class="d-flex flex-column justify-content-center align-items-center" style="font-family: 'Erica One', cursive;">
-    <div>
-        <h1 style='color:#1F2C47;' class="p-5">Seu email de verificação está a caminho!</h1>
-        <img style="width:40vw" src="{{asset('/images/1552354.svg')}}" alt="Image"/>
-    </div>
+<div class="d-flex flex-column justify-content-center align-items-center container shadow p-5 h-100" style="font-family: 'Oswald', sans-serif; color:#1F2C47;">
+        <img src="https://img.icons8.com/external-vitaliy-gorbachev-flat-vitaly-gorbachev/100/000000/external-paper-plane-support-vitaliy-gorbachev-flat-vitaly-gorbachev.png"/>
+        <h1 class="pt-5">Verifique seu email!</h1>
+        <p class="fs-3 text-center mt-3">Enviamos para o seu email um link de verificação para ativar a sua conta. Acesse o mesmo e resgate num período de até 24 horas (porque sim) para poder aproveitar todos os recursos do sistema. De outra forma, nós iremos inativar a sua conta e revogar os seus direitos de acesso (necessário para gerar texto para ficar menos feio) </p>
 </div>
 
 
